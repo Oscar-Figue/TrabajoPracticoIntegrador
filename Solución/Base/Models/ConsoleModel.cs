6886f96c080d3359116eb5c06e3b3a9c0a8983d0
TottiFigueroa <49458606+TottiFigueroa@users.noreply.github.com>
2023-12-30 21:23:51 -0300

    Implementación ABMC

--- Solución/Base/Models/ConsoleModel.cs
+++ Solución/Base/Models/ConsoleModel.cs
@@ -8,7 +8,17 @@ namespace Base.Models
 {
     public class ConsoleModel
     {
+        public ConsoleModel() { }
+        public ConsoleModel(string name)
+        {
+            this.Nombre = name;
+        }
         public int Id { get; set; }
         public string Nombre { get; set; }
+
+        public string ConsoleString
+        {
+            get { return $"#{Id}|{Nombre}"; }
+        }
     }
 }
---
+++ Solución/TPIntegrador/Views/Consoles/FrmConsoleForm.cs
@@ -0,0 +1,59 @@
+﻿using Base.Base;
+using Base.Helpers;
+using Base.Models;
+using Repository.Entities;
+using Services.Services;
+using System;
+using System.Data;
+using System.Linq;
+
+namespace TPIntegrador.Views
+{
+    public partial class FrmConsolesForm : FrmBase
+    {
+        private ConsoleService _consoleService;
+        private ConsoleModel _editModel;
+        public FrmConsolesForm(UserModel loggedUser, ConsoleModel model = null) : base(loggedUser)
+        {
+            _consoleService = new ConsoleService();
+            _editModel = model;
+            InitializeComponent();
+        }
+
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            var name = txtName.Text;
+            if (string.IsNullOrEmpty(name))
+            {
+                MessageHelper.ShowError("El campo es requerido");
+                return;
+            }
+            ConsoleModel model;
+            if (_editModel != null)
+            {
+                _editModel.Nombre = name;
+                _consoleService.Update(_editModel);
+            }
+            else
+            {
+                model = new ConsoleModel(name);
+                _consoleService.Add(model);
+            }
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FrmConsolesForm_Load(object sender, EventArgs e)
+        {
+            if (_editModel != null)
+            {
+                txtName.Text = _editModel.Nombre;
+            }
+        }
+    }
+}
