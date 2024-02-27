f1ace9c912545f1e28e561035a328686886f3f72
TottiFigueroa <49458606+TottiFigueroa@users.noreply.github.com>
2024-02-26 23:54:25 -0300

    Client service y repository

---
+++ Solución/Services/Services/ClientService.cs
@@ -0,0 +1,12 @@
+﻿using Repository.Entities;
+using Services.Base;
+
+namespace Services.Services
+{
+    public class ClientService : GenericService<ClientModel, Client>
+    {
+        public ClientService()
+        {
+        }
+    }
+}
