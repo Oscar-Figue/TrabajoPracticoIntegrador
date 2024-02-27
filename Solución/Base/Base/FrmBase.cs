d6c70f5ff09aad7c8141c77ba747dd7dfddb8eca
TottiFigueroa <49458606+TottiFigueroa@users.noreply.github.com>
2024-02-27 01:10:51 -0300

    merge

--- Solución/Base/Base/FrmBase.cs
+++ Solución/Base/Base/FrmBase.cs
@@ -6,18 +6,10 @@ namespace Base.Base
     public class FrmBase : Form
     {
         UserModel UsuarioLogueado;
-
-        public GameModel LoggedUser { get; }
-
         public FrmBase(UserModel usuarioLogueado)
         {
             UsuarioLogueado = usuarioLogueado;
         }
         public FrmBase() { }
-
-        public FrmBase(GameModel loggedUser)
-        {
-            LoggedUser = loggedUser;
-        }
     }
 }
